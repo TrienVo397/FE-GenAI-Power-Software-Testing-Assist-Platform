be59f52eee52d528c4260e7a40cbfec3cd30ab9c
Tran Dai Phuc <daiphuc158@gmail.com>
2025-07-28 11:36:20 +0700

    refactor: restructure GoogleLoginButton component for improved readability

--- src/components/login/GoogleLoginButton.tsx
+++ src/components/login/GoogleLoginButton.tsx
@@ -10,12 +10,14 @@ const GoogleLoginButton: React.FC = () => {
 
   return (
     <Button variant="outline" className="w-full flex items-center justify-center gap-2">
-      <img
-        src="https://www.svgrepo.com/show/475656/google-color.svg"
-        alt="Google"
-        className="h-5 w-5"
-      />
-      Continue with Google
+      <span className="flex items-center gap-2">
+        <img
+          src="https://www.svgrepo.com/show/475656/google-color.svg"
+          alt="Google"
+          className="h-5 w-5"
+        />
+        <span>Continue with Google</span>
+      </span>
     </Button>
   );
 };
